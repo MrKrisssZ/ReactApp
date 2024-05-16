e02178198bf8cab8ce639efc2c2f6c062836204c
Yesheng Zhong <yesheng.zhong@student.adelaide.edu.au>
2024-05-17 01:27:51 +0930

    Create Button

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,9 +1,13 @@
 function Home(){
 
-
+    const handleClick = (name, e) => {
+        console.log('Hello! ' + name, e.target)
+    }
     return (
+        // if add handleClick(), then it will automatically invoke the click without user actually click it.
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={(e) => {handleClick('kris', e)}}>Click Me</button>
         </div>
     );
 }
